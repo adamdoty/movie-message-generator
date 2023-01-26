c065f148ab71c06462dd43345c479749306607fb
Adam Doty <acdoty15@gmail.com>
2023-01-26 09:45:49 -0800

    Write compose message

---
+++ generate-message.js
@@ -0,0 +1,21 @@
+const data = require('./MOCK_DATA (1).json')
+
+const getRandomDataEntry = () => {
+  return data[Math.floor(Math.random() * data.length)]
+}
+
+const formatQuote = (quote) => {
+  return quote.charAt(0).toUpperCase() + quote.slice(1) + '.'
+}
+
+const composeMessage = (messageData) => {
+  // format quote
+  const message = `
+    Movie Quote of the Day: "${formatQuote(messageData.movie_quote)}" 
+    - ${messageData.quotee}\n
+    Movie Genre of the Day: "${messageData.movie_genre}"\n
+    Movie of the Day: "${messageData.movie_title}"`
+  return message
+}
+
+console.log(composeMessage(getRandomDataEntry()))
